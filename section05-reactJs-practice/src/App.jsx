17345430e11115241a6509a3c749faed0ecf43dd
hyungjin kim <hjux07@gmail.com>
2024-12-05 20:25:42 +0900

    [⚙️chore] React Component - 리액트 컴포넌트 구조 - App.jsx는 Root Component

--- section05-reactJs-practice/src/App.jsx
+++ section05-reactJs-practice/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
 import './App.css'
+import Header  from './components/Header'; // 파일을 찾아가도록 내부적으로 자동 설정이 되어 있으므로 확장자 생략 가능
+import Main from './components/Main';
+import Footer from './components/Footer';
 
+
+// * Root Component
 function App() {  
 
   return (
     <>
-      <h1>Hello React!</h1>
+    {/* 자식 컴포넌트 */}
+      <Header/>      
+      <Main/>
+      <Footer/>            
     </>
   )
 }
 
-export default App
+export default App;
