e75f5ba22a8ef4f90f612aad37efcbb40356cbf6
hyungjin kim <hjux07@gmail.com>
2024-12-06 20:18:31 +0900

    [✨feat] Create component variable with `useRef`  - `useRef()`:    1. Reference 객체를 생성    2. 컴포넌트 내부의 변수로 활용 가능    3. 어떤 경우에도 리렌더링을 유발하지 않음    4. 특정 DOM요소에 접근 가능, 요소 조작 가능    5. 리렌더링을 유발시키지 않는다.      5.1. 컴포넌트 내부에서 렌더링에 영향을 미치지 않음

--- section05-reactJs-practice/section05.md
+++ section05-reactJs-practice/section05.md
@@ -90,19 +90,42 @@
 
 ### `state`로 사용자 입력 관리하기
 
-```jsx
-// * 비슷한 여러 개의 state가 있을 때 하나의 객체값으로 묶어서 하나의 state로 통합해서 관리해준다.
-const [userInfo, setUserInfo] = useState({
-  name: '이름',
-  birth: '',
-  country: '',
-  bio: '',
-});
-
-// * 비슷한 이벤트 핸들러를 통합 이벤트 핸들러로 묶어준다.
-const onChangeUserInfo = ({ target: { value, name } }) =>
-  setUserInfo({
-    ...userInfo,
-    [name]: value,
+  ```jsx
+  // * 비슷한 여러 개의 state가 있을 때 하나의 객체값으로 묶어서 하나의 state로 통합해서 관리해준다.
+  const [userInfo, setUserInfo] = useState({
+    name: '이름',
+    birth: '',
+    country: '',
+    bio: '',
   });
-```
+
+  // * 비슷한 이벤트 핸들러를 통합 이벤트 핸들러로 묶어준다.
+  const onChangeUserInfo = ({ target: { value, name } }) =>
+    setUserInfo({
+      ...userInfo,
+      [name]: value,
+    });
+  ```
+
+
+
+
+
+- `useRef()`: 
+  1. Reference 객체를 생성 
+  2. 컴포넌트 내부의 변수로 활용 가능
+  3. 어떤 경우에도 리렌더링을 유발하지 않음
+  4. 특정 DOM요소에 접근 가능, 요소 조작 가능
+  5. 리렌더링을 유발시키지 않는다.
+    5.1. 컴포넌트 내부에서 렌더링에 영향을 미치지 않음
+  
+  렌더링에 영향을 미치고 싶지 않은 그러한 변수를 생성할 때 이 `useRef()`를 이용하게 된다.
+  
+  `useState()`, `useRef()`로 생성한 특수한 변수들을 컴포넌트가 리렌더링 된다고 해도
+  다시 리셋이 되지 않는다. (내부족으로 그렇게 설계가 되어 있다.)
+
+  컴포넌틑 내부의 변수가 필요하다면 일반 변수가 아닌 `useRef()`를 사용하거나
+  리렌더링에 영향을 주고 싶다면 `useState()`로 만들어야 한다.
+
+  그냥 일반 변수를 선언을 할 경우 해당 컴포넌트가 리렌더링 될 때
+  해당 컴포넌트인 함수를 다시 실행하는 것이므로 변수가 초기화 되기 때문이다.
--- section05-reactJs-practice/src/components/Register.jsx
+++ section05-reactJs-practice/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 // 회원가입 폼
 // 1. 이름
@@ -9,27 +9,50 @@ import React, { useEffect, useState } from 'react';
 export const Register = () => {
   // * 비슷한 여러 개의 state가 있을 때 하나의 객체값으로 묶어서 하나의 state로 통합해서 관리해준다.
   const [userInfo, setUserInfo] = useState({
-    name: '이름',
+    name: '',
     birth: '',
     country: '',
     bio: '',
   });
 
-  useEffect(() => {
-    console.log(userInfo);
-  }, [userInfo]);
+  const countRef = useRef(0);  
+
+  const inputRef = useRef(null);
+  
+  
+
+  // useEffect(() => {
+  //   console.log(userInfo);
+  //   console.log(refObj.current);
+    
+  // }, [userInfo]);
+
 
   // * 비슷한 이벤트 핸들러를 통합 이벤트 핸들러로 묶어준다.
-  const onChangeUserInfo = ({ target: { value, name } }) =>
+  const onChangeUserInfo = ({ target: { value, name } }) =>{
+    
+    // 수정 횟수 카운트
+    console.log(countRef.current++);
+
     setUserInfo({
       ...userInfo,
-      [name]: value,
+      [name]: value,      
     });
+  };
+
+  const onSubmit = () =>{
+    if(userInfo.name === ''){
+      // 이름을 입력하는 DOM요소 focus
+      inputRef.current.focus();      
+    }        
+  }
+
 
   return (
     <div>
+
       <div>
-        <input value={userInfo['name']} name="name" onChange={onChangeUserInfo} placeholder={'이름'} />
+        <input ref={inputRef} value={userInfo['name']} name="name" onChange={onChangeUserInfo} placeholder={'이름'} />
       </div>
 
       <div>
@@ -48,6 +71,8 @@ export const Register = () => {
       <div>
         <textarea value={userInfo['bio']} name="bio" onChange={onChangeUserInfo}></textarea>
       </div>
+
+      <button onClick={onSubmit} >제출</button>
     </div>
   );
 };
