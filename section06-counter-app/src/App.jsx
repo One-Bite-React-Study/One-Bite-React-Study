16dfade77cf66a0092e80f5f1c4aec218c664d34
hyungjin kim <hjux07@gmail.com>
2024-12-06 21:33:41 +0900

    [✨feat] Implementing Counter App 1. 리액트에서 화면을 구성할 때 여러 개의 컴포넌트들이 서로 부모와 바식 관계를 이루며 계층 구조를 형성한다. 2. 특정 컴포넌트가 다른 컴포넌트에게 데이터를 전달하려면 반드시 두 컴포넌트는 서로 부모 자식 관계를 가지고 있어야 한다. 3. 그렇기 때문에 하나의 `state`를 여러 컴포넌트에서 관리하게 될 경우 이 `state`는 반드시 컴포넌트들의 공통 부모가 되는 곳에 만들어야 한다.   3.1. `state`를 계층 구조사에서 위로 끌어올려서 그 아래에 있는 컴포넌트들이 모두 공유할 수 있도록 만드는 방법을         State Lifting(State 끌어 올리기)라고 표현한다.   3.2. 리액트에서는 `props`라는 기능을 이용해서 부모에서 자식 방향으로만 데이터를 데이터를 전달할 수 있다.        그래서 그렇기 때문에 데이터들을 항상 위에서 아래로 하나의 방향으로만 흐르게 된다.(단방향 데이터 흐름)

--- section06-counter-app/src/App.jsx
+++ section06-counter-app/src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css';
+import React, { useState } from 'react'
 import { Viewer, Controller } from './components/_index';
 
+
+// * 부모에서 자식 컴포넌트에 `props`로 `state`를 공유해야 하므로 `<App/>` 컴포넌트에 `state`를 생성
+// -> 형제 관계인 컴포넌트 끼리는 서로 `props`로 값을 공유할 수 없다.
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  // 이벤트 핸들러를 부모 컴포넌트 내부에서 생성해서 `props`로 전달 
+  const onClickButton = (value) => () => setCount(count + value);
+
   return (
     <div className='App'>
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
 
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton} />
       </section>
     </div>
   );
--- section06-counter-app/src/components/Controller.jsx
+++ section06-counter-app/src/components/Controller.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-export const Controller = () => {
+export const Controller = ({onClickButton}) => {
+
+  const values = [ -1, -10, -100, 100, 10, 1];
+
   return (
     <div>
-      <button>-1</button>
-      <button>-10</button>
-      <button>-100</button>
-      <button>+100</button>
-      <button>+10</button>
-      <button>+1</button>
+      {
+        values.map((value, i)=>
+          <button key={i} onClick={onClickButton(value)}>{value}</button>
+        )
+      }
     </div>
   );
 };
