344545f75bb1f1832976ecaa2610b97d0d383a6c
hyungjin kim <hjux07@gmail.com>
2024-12-07 22:19:24 +0900

    [⚙️chore] Create mockup data and Set status - id, 체크 여부, 콘텐츠, 날짜 속성들을 가진 객체를 목업 데이터로 생성 후 배열에 담아 state 설정

--- section08-to-do-list-app/src/App.jsx
+++ section08-to-do-list-app/src/App.jsx
@@ -1,7 +1,36 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Header, Editor, List } from './components/_index';
+import React, { useState } from 'react';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React Study',
+    data: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: 'Doing laundry',
+    data: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: 'Sining',
+    data: new Date().getTime(),
+  },
+];
 
 function App() {  
+  const [todos, setTodos] = useState(mockData);
+
+  useEffect(()=>{
+    console.log(todos);
+    
+  }, [])
 
   return (
     <div className='App'>
