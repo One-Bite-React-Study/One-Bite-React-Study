e7045c64d8df0ab3f2e4ef8e884a59367ef78fb9
hyungjin kim <hjux07@gmail.com>
2024-12-08 11:57:59 +0900

    [✨feat] Read - To do list rendering 2 1. input에 onChange가 실행될때 마다 해당 값을  `search` state에 설정
    2. 컴포넌트가 리렌더링될 때 마다 실행되어 todos 값을 필터링할 함수 `getFilteredData` 선언
      2.1. 만약 `search`가 빈 문자열이면 `todos`를 반환
      2.2. `search` state가 변경되어 해당 컴포넌트가 리렌더링(Update)될 때 마다 `todos`를 필터링해서 반환
    
    3. 컴포넌트가 리렌더링(Update) 될 때 마다 다시 실행되므로 필터링된 반환 값을 화면에 뿌려준다.

--- section08-to-do-list-app/src/components/List.jsx
+++ section08-to-do-list-app/src/components/List.jsx
@@ -3,27 +3,31 @@ import { TodoItem } from './_index';
 import './List.css';
 
 export const List = ({ todos }) => {
-  const [search, setSearch] = useState(todos);
+  const [search, setSearch] = useState('');
 
-  const getFilteredData = ({ target: { value } }) => {
+  // input에 onChange가 실행될때 마다 해당 값을  `search` state에 설정 
+  const onChangeSearch = ({target: { value }}) => setSearch(value);
 
-    if(search === '') {
-      alert('검색어를 입력해주세요.')
-      return;
-    }
-    
-    const filteredData = todos.filter(({ content }) => content.toLowerCase().includes(value.toLowerCase()));
+  // - 만약 `search`가 빈 문자열이면 `todos`를 반환
+  // - `search` state가 변경되어 해당 컴포넌트가 리렌더링(Update)될 때 마다 `todos`를 필터링해서 반환
+  const getFilteredData = () => {
 
-    setSearch(filteredData);
+    if(search === '') return todos;
+
+    return todos.filter(({ content }) => content.toLowerCase().includes(search.toLowerCase()));    
   };
+
+  // 컴포넌트가 리렌더링(Update) 될 때 마다 다시 실행되므로 필터링된 반환 값을 화면에 뿌려준다.
+  const filteredTodos = getFilteredData();
 
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input onChange={getFilteredData} type="text" placeholder="검색어를 입력하세요" />
+      <input onChange={onChangeSearch} value={search} type="text" placeholder="검색어를 입력하세요" />
 
       <div className="todos_wrapper">
-        {search.map(({ id, ...todo }) => (
+        {/* 필터링된 todos를 화면에 뿌려준다. */}
+        {filteredTodos.map(({ id, ...todo }) => (
           <TodoItem key={id} {...todo} />
         ))}
       </div>
