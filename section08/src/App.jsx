6ae93c46042f75d125b99ed6e5a94e21f0a99d09
이한빈 <kx1302@naver.com>
2024-12-10 21:05:19 +0900

    feat: 8.2) UI 구현하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+import List from './components/List';
+import Editor from './components/Editor';
 
 function App() {
-
   return (
     <>
-      TodoList  
+              <div className='App'>
+        <Header />
+        
+
+        
+    
+          <Editor />
+      <List />
+    </div>
     </>
   )
 }
 
-export default App
+export default App;
